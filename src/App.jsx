e09f9c9f55bdca7febffe1e884c44bf5097e38c5
germanavaz <gmsvaz@gmail.com>
2025-01-20 22:53:39 -0300

    :tada: first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import './Style.scss';
+
+export default function App() {
+
+  const ganhadores24 = [
+    {
+      img:"https://upload.wikimedia.org/wikipedia/pt/8/82/Barbie_%282023%29.jpg",
+      altImg: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus totam quasi deserunt!",
+      titulo: "Barbie",
+      premios: "Melhor trilha sonora",
+      descricao: "  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus totam quasi deserunt cum cupiditate sint minus enim, quidem voluptatum, in numquam! Sunt quas dolore iusto!",
+      ondeAssistir: "Netflix",
+      ondeAssistirLink: "https://www.netflix.com"
+    }
+  ];
+
+  return(
+  <>
+  <h1>Oscarflix</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, illum?</p>
+
+  
+  <section id='vencedores24'>
+  <h2>Vencedores Oscar 2024</h2>
+    <div>
+    {ganhadores24.map((index, id) => (
+      <article key={id}>
+        <img src={index.img} alt={index.altImg} />
+        <div>
+          <h4>{index.premios}</h4>
+          <h3>{index.titulo}</h3>
+          <p>{index.descricao}</p>
+          <p>Onde assistir: <a href={index.ondeAssistirLink} target="_blank" rel="noopener noreferrer">{index.ondeAssistir}</a></p>
+        </div>
+      </article>
+    ))}
+    </div>
+
+  </section>
+
+  </>)
+}
